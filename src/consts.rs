e63205bbbe8d5d627e40dbfcf410eabcb4afc6bc
bigzhu <vermiliondun@gmail.com>
2024-01-12 15:03:23 +0800

    add bitfinex UST

--- src/consts.rs
+++ src/consts.rs
@@ -8,6 +8,7 @@ lazy_static! {
         m.insert("SOL".to_string(), 1.0);
         m.insert("BTC".to_string(), 0.00002);
         m.insert("USDT".to_string(), 150.0);
+        m.insert("UST".to_string(), 150.0);
         m
     };
 }
